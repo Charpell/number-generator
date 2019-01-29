da35437df22a79432d438acd9dd1e018c703c0ac
Dannytebj <Dannytebj@gmail.com>
2019-01-29 13:31:45 +0100

    setup angular app

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const { numbersController } = require('./numbersController');
-const routes = require('./router');
+const routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname+'/dist/number-generator'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/number-generator/index.html'))
+})
 app.use('/api/v1', routes);
 
 // start the server in the port 3333 !
